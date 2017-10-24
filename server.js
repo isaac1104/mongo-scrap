58c78773dc038548b80a2f9974c29ca1519a3587
Isaac Kwon <you@example.com>
2017-10-23 20:26:49 -0700

    Scraped from NYT

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+const cheerio = require("cheerio");
+const mongojs = require("mongojs");
+const request = require("request");
 const express = require("express");
 const bodyParser = require("body-parser");
 const PORT = 3000;
@@ -13,6 +16,49 @@ app.set("view engine", "handlebars");
 
 app.get("/", (req, res) => {
   res.render("index.handlebars");
+});
+
+const databaseUrl = "nyt-scraper";
+const collections = ["scrapedData"];
+
+const db = mongojs(databaseUrl, collections);
+db.on("error", (error) => {
+  console.log(`Database Error: ${error}`);
+});
+
+app.get("/all", (req, res) => {
+  db.scrapedData.find({}, (error, data) => {
+    if (error) {
+      return console.log(error);
+    } else {
+      res.json(data);
+    }
+  });
+});
+
+app.get("/scrape", (req, res) => {
+  request("https://www.nytimes.com/section/sports/basketball", (error, response, html) => {
+    const $ = cheerio.load(html);
+    $(".story-meta").each((i, element) => {
+      const title = $(element).children(".headline").text();
+      const summary = $(element).children(".summary").text();
+
+      if (title && summary) {
+        db.scrapedData.insert({
+          title,
+          summary
+        },
+        function(err, data) {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log(data);
+          }
+        });
+      }
+    });
+  });
+  res.redirect("/");
 });
 
 app.listen(PORT, () => {
